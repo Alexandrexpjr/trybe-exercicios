0ae8dd53efbcda156264c33f982823c59b3838cc
alexandrexpj <alexandrexpjr@hotmail.com>
2021-07-28 19:42:44 -0300

    adiciona exercicios 4,5,6

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-java-script-primeiros-passos/exercicios-finais/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-java-script-primeiros-passos/exercicios-finais/scripts.js
@@ -43,3 +43,60 @@ if (numberOne > numberTwo && numberOne > numberThree) {
 
 // 4) Positivo, negativo ou nulo;
 
+let number = -1;
+
+console.log("Resultados do Quarto Exercício:");
+if (number > 0) {
+    console.log("positive");
+} else if (number < 0) {
+    console.log("negative");
+} else {
+    console.log("zero");
+}
+
+// 5) Ângulos internos do triângulo;
+
+let angleA = 79;
+let angleB = 60;
+let angleC = 41;
+let sum = angleA + angleB + angleC;
+
+console.log("Resultados do Quinto Exercício:");
+if (angleA <=0 || angleB <=0 || angleC <=0) {
+    console.log("Ângulo inválido");
+} else if (sum == 180) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+// 6) Peças de xadrez e seus movimentos;
+
+let piece = 'kniGHT';
+let pieceLower = piece.toLowerCase()
+
+console.log("Resultados do Sexto Exercício:");
+
+switch (pieceLower) {
+    case 'pawn':
+        console.log("Moves only forward, one square at time, excepts for its first move, which moves two squares. Eat diagonally.");
+        break;
+    case 'knight':
+        console.log("Moves in L-shape, two squares for any direction, then one perpendicular square . Can move over pieces");
+        break;
+    case 'bishop':
+        console.log("Moves diagonally, through any number of unoccupied squares.");
+        break;
+    case 'rook':
+        console.log("Moves vertically or horizontally, through any number of unoccupied squares");
+        break;
+    case 'queen':
+        console.log("Moves either diagonally,vertically or horizontally, through any number of unoccupied squares.");
+        break;
+    case 'king':
+        console.log("Moves one square for any direction, except in castling")
+        break;
+    default:
+        console.log("error: invalid piece")        
+}
+
