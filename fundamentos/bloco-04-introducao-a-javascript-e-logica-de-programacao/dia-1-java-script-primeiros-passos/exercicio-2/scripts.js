882a66db5e8d562d877061c0f40fa117bf67899b
alexandrexpj <alexandrexpjr@hotmail.com>
2021-07-28 16:24:52 -0300

    adiciona exercicios da aula assincrona

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-java-script-primeiros-passos/exercicio-2/scripts.js
@@ -0,0 +1,22 @@
+/* let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+patientId = "50";
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge); */
+
+let base = 5;
+let altura = 8;
+let perimetro = 2 * base + 2 * altura;
+let area = base * altura;
+
+console.log(perimetro);
+console.log(area);
