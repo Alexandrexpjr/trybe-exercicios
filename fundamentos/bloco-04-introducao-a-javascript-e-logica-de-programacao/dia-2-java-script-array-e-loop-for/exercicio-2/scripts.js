abf6c58838d4caf52c6c9d54922cb66035b8a500
alexandrexpj <alexandrexpjr@hotmail.com>
2021-07-29 15:46:18 -0300

    adiciona exercicio 2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicio-2/scripts.js
@@ -0,0 +1,8 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+groceryList.push('Batata', 'Cenoura', 'Rúcula') ;
+
+for (index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
+
