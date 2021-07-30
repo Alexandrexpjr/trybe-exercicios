bfb95f45c17ffd70bbf58403f801ca69d3870ae4
alexandrexpj <alexandrexpjr@hotmail.com>
2021-07-30 15:40:14 -0300

    adiciona baby steps aos exercicios 1 e 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicio-2/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicio-2/scripts.js
@@ -17,3 +17,17 @@ for (let nomes of names) {
     nomes += ' Pereira'; //para colocar sobrenome em todo mundo xD
     console.log(nomes);
 }
+
+
+//baby steps: Primeira parte:
+//1- criar array groceryList
+//2- adicionar três novas strings ao fim do array
+//3- Criar um loop que percorre o array
+//4- Imprime o conteúdo do array em linhas diferentes
+
+//segunda parte:
+//1- criar array nomes;
+//2- Imprimir uma frase para indicar que passamos para a segunda parte do exercicio;
+//3- Criar um loop que percorre o array names;
+//4- adicionar o sobrenome Pereira a todos os nomes;
+//5- Imprimir os nomes com sobrenome;
