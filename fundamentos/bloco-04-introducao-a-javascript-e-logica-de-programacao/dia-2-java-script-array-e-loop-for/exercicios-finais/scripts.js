8a545e36def7a58262802738209dc598a4c67362
alexandrexpj <alexandrexpjr@hotmail.com>
2021-07-30 15:57:37 -0300

    adiciona babysteps nos ex finais

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicios-finais/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-java-script-array-e-loop-for/exercicios-finais/scripts.js
@@ -1,11 +1,17 @@
+// 1-Criar array numbers;
+// 2-Criar um loop que percorre o array;
+// 3-Imprimir os valores nele contidos;
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (i = 0; i < numbers.length; i += 1) {
+    console.log(numbers[i]);
+}
 
-// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
-
-console.log(numbers);
-console.log();
-
-// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+// 1- Criar a variável soma;
+// 2- Atribuir o valor 0 para ela;
+// 3- Criar um loop que percorre o array numbers;
+// 4- Somar todos os valores do array e armazenar na variável soma;
+// 5- Imprimir o resultado;
 
 let soma = 0;
 for (i = 0; i < numbers.length; i += 1) {
@@ -15,14 +21,17 @@ for (i = 0; i < numbers.length; i += 1) {
 console.log("A soma de todos os números é de " + soma);
 console.log();
 
-// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
-// A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+// 1- Criar a variável media;
+// 2- Atribuir o seu valor como sendo a variável soma dividido pelo tamanho do array numbers;
+// 3- Imprimir o resultado;
 
 let media = soma / numbers.length;
 console.log("A média aritmética dos valores contidos no array é de " + media);
 console.log();
 
-// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+// 1- Criar um if com a condição da variável media ser maior que 20;
+// 2- Se for maior que 20, imprimir "valor maior que 20";
+// 3- Caso contrário, imprimir "valor menor ou igual a 20";
 
 if (media > 20) {
     console.log("valor maior que 20")
@@ -33,6 +42,11 @@ if (media > 20) {
 console.log();
 
 // Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+// 1- Criar uma variável maiorNumero;
+// 2- Atribuir o primeiro número do array numbers à ela;
+// 3- Criar um loop que percorre o array;
+// 4- Se o valor atual for maior que o armazenado na variavel maiorNumero, a variável assume o novo valor;
+// 5- Imprime o valor do maior número encontrado.
 
 maiorNumero = numbers[0];
 for (i = 0; i < numbers.length; i += 1) {
