4d752bcd393902203d88f2455a5d4471e4a4380a
alexandrexpj <alexandrexpjr@hotmail.com>
2021-08-30 19:03:34 -0300

    finaliza parte2

--- fundamentos/bloco-07-javascript-ES6/dia-2-objects/exercicios-finais/script-parte2.js
+++ fundamentos/bloco-07-javascript-ES6/dia-2-objects/exercicios-finais/script-parte2.js
@@ -55,14 +55,23 @@ const allLessons = {
 
 console.log(allLessons);
 
-// Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
-// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
-// Copiar
-// console.log(getValueByNumber(lesson1, 0));
-// // Output: 'Matématica'
-// Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
-// Copiar
-// console.log(verifyPair(lesson3, 'turno', 'noite'));
-// // Output: true,
-// console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
-// // Output: false
+// 6) Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+const allStudents = object => {
+  return `Número total de estudantes: ${object.lesson1.numeroEstudantes + object.lesson2.numeroEstudantes + object.lesson3.numeroEstudantes}`;
+}
+
+console.log(allStudents(allLessons));
+// 7) Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+const getValueByNumber = (object, position) => {
+  return Object.values(object)[position];
+}
+
+console.log(getValueByNumber(lesson1, 0));
+
+// 8) Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
+const verifyPair = (object, key, value) => {
+  const savedValue = object[key];
+  return (value === savedValue) ? true : false;
+}
+
+console.log(verifyPair(lesson3, 'turno', 'noite'));
