6b1480f32eea20aa36deb803cfbde9bb45c4236d
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-01 10:29:04 -0300

    exercicio2

--- fundamentos/bloco-07-javascript-ES6/dia-3-testes-unitarios/exercicios-finais/exercicio2.js
+++ fundamentos/bloco-07-javascript-ES6/dia-3-testes-unitarios/exercicios-finais/exercicio2.js
@@ -4,7 +4,7 @@
 // Verifique se o array passado por parâmetro não sofreu alterações
 // Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
 
-// const assert = require('assert');
+const assert = require('assert');
 
 function myRemove(arr, item) {
     let newArr = [];
@@ -17,4 +17,7 @@ function myRemove(arr, item) {
 }
 
 // implemente seus testes aqui
-console.log(myRemove([1, 2, 3, 4], 3));
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemove([1, 2, 3]), [1, 2, 3]);
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
