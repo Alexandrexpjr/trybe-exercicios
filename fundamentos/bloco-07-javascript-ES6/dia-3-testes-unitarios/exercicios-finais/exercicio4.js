09d1809ff3d435f17d760ebd82956b7973f3127c
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-01 10:59:46 -0300

    Exercicio 4

---
+++ fundamentos/bloco-07-javascript-ES6/dia-3-testes-unitarios/exercicios-finais/exercicio4.js
@@ -0,0 +1,25 @@
+// 4) A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número
+// Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
+// Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado
+
+const assert = require('assert');
+
+function myFizzBuzz(num) {
+    if (typeof num !== 'number') return false;
+    if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+    if (num % 3 === 0) return 'fizz';
+    if (num % 5 === 0) return 'buzz';
+    return num;
+}
+
+// implemente seus testes aqui
+
+assert.strictEqual(typeof myFizzBuzz, 'function');
+assert.strictEqual(myFizzBuzz(60), 'fizzbuzz');
+assert.strictEqual(myFizzBuzz(27), 'fizz');
+assert.strictEqual(myFizzBuzz(20), 'buzz');
+assert.strictEqual(myFizzBuzz(47), 47);
+assert.strictEqual(myFizzBuzz('oi'), false);
