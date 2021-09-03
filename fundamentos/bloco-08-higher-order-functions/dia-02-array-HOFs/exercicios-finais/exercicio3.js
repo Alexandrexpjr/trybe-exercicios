40b443b3a8eff49021043098a6315202e7910a06
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-03 18:53:08 -0300

    exercicio 3

--- fundamentos/bloco-08-higher-order-functions/dia-02-array-HOFs/exercicios-finais/exercicio3.js
+++ fundamentos/bloco-08-higher-order-functions/dia-02-array-HOFs/exercicios-finais/exercicio3.js
@@ -64,3 +64,22 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  // escreva seu código aqui
+  const book = books.find(book => book.name.length === 26)
+  return book;
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
