7414e6f43dbe1e79b0e7ec5d73952cfc6430a617
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-03 16:18:22 -0300

    exercicio fixacao

---
+++ fundamentos/bloco-08-higher-order-functions/dia-02-array-HOFs/exercicio-fixacao/find.js
@@ -0,0 +1,24 @@
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+
+console.log(findDivisibleBy3And5());
+
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => names.find((name => name.length === 5));
+
+console.log(findNameWithFiveLetters());
+
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+  
+function findMusic(id) {
+    return musicas.find((music) => music['id'] === id)
+}
+  
+console.log(findMusic('31031685'))
