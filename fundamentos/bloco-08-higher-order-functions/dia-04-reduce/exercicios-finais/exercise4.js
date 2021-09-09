36492eeadd78d03f9196d8a6d10a291b9a98d1f7
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-09 19:49:35 -0300

    exercicio 4

--- fundamentos/bloco-08-higher-order-functions/dia-04-reduce/exercicios-finais/exercise4.js
+++ fundamentos/bloco-08-higher-order-functions/dia-04-reduce/exercicios-finais/exercise4.js
@@ -64,3 +64,21 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+}
+
+function longestNamedBook() {
+  // escreva seu código aqui
+  return books.reduce((biggerName, actualName) => (actualName.name.length > biggerName.name.length) ? actualName : biggerName);
+}
+
+assert.deepStrictEqual(longestNamedBook(), expectedResult);
