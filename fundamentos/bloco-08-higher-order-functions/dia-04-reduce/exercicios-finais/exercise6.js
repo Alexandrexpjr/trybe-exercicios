db1dc5a0a66655888aad8c1aa3d39682064b5673
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-10 13:19:22 -0300

    finalmente exercicio6 UHU

--- fundamentos/bloco-08-higher-order-functions/dia-04-reduce/exercicios-finais/exercise6.js
+++ fundamentos/bloco-08-higher-order-functions/dia-04-reduce/exercicios-finais/exercise6.js
@@ -1,13 +1,18 @@
 // Agora vamos criar um novo array de objetos a partir das informações abaixo, onde cada objeto terá o formato { name: nome do aluno, average: media das notas } . Para isso vamos assumir que a posição 0 de notes refere-se ao aluno na posição 0 de students , aqui além de reduce será necessário utilizar também a função map . Dica: Você pode acessar o index do array dentro de map , e você pode ver o objeto esperado na constante expected .
 
 const assert = require('assert');
+const { arrayBuffer } = require('stream/consumers');
 
 const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
   // escreva seu código aqui
-  
+  let newArray = [];
+  const tente = grades.forEach((grade, index) => {
+    newArray[index] = grade.reduce(((sum, current) => sum + current), 0) / grade.length;
+  })
+  return students.map((name, index) => ({name: name, average: newArray[index]}));
 }
 
 const expected = [
