1674a0d433549e8c3b4e77b5d080d01b8c6b570c
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-15 18:38:59 -0300

    exercicios 1-4

---
+++ fundamentos/bloco-09-js-assincrono-e-promises/dia-01-js-assincrono-e-callbacks/exercicios-finais/exercise3.js
@@ -0,0 +1,29 @@
+// Primeiro jeito
+// const getPlanet = () => {
+//   setTimeout(() => {
+//   const mars = {
+//     name: "Mars",
+//     distanceFromSun: {
+//       value: 227900000,
+//       measurementUnit: "kilometers",
+//     },
+//   };
+//   console.log("Returned planet: ", mars);
+//   }, 4000)
+// };
+
+// getPlanet(); // imprime Marte depois de 4 segundos
+
+// Segundo jeito
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  console.log("Returned planet: ", mars);
+};
+
+setTimeout(getPlanet, 4000);
