2515b1c5437ce40d829748f2e1feac52f2269e9b
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-23 11:25:20 -0300

    exercicios 4 - 6

---
+++ fundamentos/bloco-10-testes-automatizados-jest/dia-02-testes-assincronos/exercicios-finais/exercicio4.test.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+
+const getRepos = (url) => {
+  return fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      return data.map((repo) => repo.name);
+    });
+};
+
+it('gets a list of repositories names', () => {
+  const url = 'https://api.github.com/orgs/tryber/repos';
+
+  return getRepos(url).then(result => {
+    expect(result).toContain('sd-01-week4-5-project-todo-list');
+    expect(result).toContain('sd-01-week4-5-project-meme-generator');
+  });
+});
+
