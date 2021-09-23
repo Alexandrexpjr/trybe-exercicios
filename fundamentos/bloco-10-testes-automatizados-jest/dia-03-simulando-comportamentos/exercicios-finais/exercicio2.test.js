f32206e6fb0024a86d5b63f1a71d13144d8cf546
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-23 18:53:18 -0300

    exercicios 1-3

---
+++ fundamentos/bloco-10-testes-automatizados-jest/dia-03-simulando-comportamentos/exercicios-finais/exercicio2.test.js
@@ -0,0 +1,2 @@
+// Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários. 
+
