e78df9691fde6d7cafac69263a61f94020f54937
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-23 15:14:58 -0300

    exercicios da aula assinc

---
+++ fundamentos/bloco-10-testes-automatizados-jest/dia-03-simulando-comportamentos/exercicios-iniciais/mock-funcoes/mock.test.js
@@ -0,0 +1,21 @@
+const service = require('./service');
+
+test("#randomRgbColor", () => {
+  // testando quantas vezes a função foi chamada e qual seu retorno
+  service.randomRgbColor = jest
+    .fn()
+    .mockReturnValue('default value')
+    .mockReturnValueOnce('first call')
+    .mockReturnValueOnce('second call');
+
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(0);
+
+  expect(service.randomRgbColor()).toBe("first call");
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(1);
+
+  expect(service.randomRgbColor()).toBe("second call");
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(2);
+
+  expect(service.randomRgbColor()).toBe("default value");
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(3);
+});
