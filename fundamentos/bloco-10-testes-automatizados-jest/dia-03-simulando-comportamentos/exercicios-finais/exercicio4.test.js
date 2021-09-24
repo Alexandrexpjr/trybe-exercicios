5aa5f7e86aabe02b5ab735d60ea7e4869e53f00f
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-23 23:53:17 -0300

    exercicio4

--- fundamentos/bloco-10-testes-automatizados-jest/dia-03-simulando-comportamentos/exercicios-finais/exercicio4.test.js
+++ fundamentos/bloco-10-testes-automatizados-jest/dia-03-simulando-comportamentos/exercicios-finais/exercicio4.test.js
@@ -1,34 +1,51 @@
-const { upperCase, startsWith, concatString } = require("./exercicio4");
+const exercicio4 = require("./exercicio4");
+jest.mock('./exercicio4');
 
 // Faça o mock do arquivo. Faça uma nova implementação para a primeira função, mas agora ela deve retornar a string em caixa baixa. Para a segunda função, uma nova implementação deve retornar a última letra de uma string. A terceira deve receber três strings e concatená-las. 
 
-jest.mock('./exercicio4');
 
 describe('Testando implementaçoes nas funções', () => {
   it('primeira função', () => {
-    upperCase.mockImplementation((string) => string.toLowerCase());
+    exercicio4.upperCase.mockImplementation((string) => string.toLowerCase());
 
-    expect(upperCase).toHaveBeenCalledTimes(0);
-    expect(upperCase('OLA TudO bEm')).toBe('ola tudo bem');
-    expect(upperCase).toHaveBeenCalled();
-    expect(upperCase).toHaveBeenCalledTimes(1);
+    expect(exercicio4.upperCase).toHaveBeenCalledTimes(0);
+    expect(exercicio4.upperCase('OLA TudO bEm')).toBe('ola tudo bem');
+    expect(exercicio4.upperCase).toHaveBeenCalled();
+    expect(exercicio4.upperCase).toHaveBeenCalledTimes(1);
   })
   it('segunda função', () => {
-    startsWith.mockImplementation((string) => string[string.length - 1]);
+    exercicio4.startsWith.mockImplementation((string) => string[string.length - 1]);
 
-    expect(startsWith).toHaveBeenCalledTimes(0);
-    expect(startsWith('OLA TudO bEm')).toBe('m');
-    expect(startsWith).toHaveBeenCalled();
-    expect(startsWith).toHaveBeenCalledTimes(1);
+    expect(exercicio4.startsWith).toHaveBeenCalledTimes(0);
+    expect(exercicio4.startsWith('OLA TudO bEm')).toBe('m');
+    expect(exercicio4.startsWith).toHaveBeenCalled();
+    expect(exercicio4.startsWith).toHaveBeenCalledTimes(1);
   });
   it('terceira função', () => {
-    concatString.mockImplementation((string1, string2, string3) => string1 + string2 + string3);
+    exercicio4.concatString.mockImplementation((string1, string2, string3) => string1 + string2 + string3);
 
-    expect(concatString).toHaveBeenCalledTimes(0);
-    expect(concatString('Ola, ', 'tudo ', 'bem')).toBe('Ola, tudo bem');
-    expect(concatString).toHaveBeenCalled();
-    expect(concatString).toHaveBeenCalledTimes(1);
+    expect(exercicio4.concatString).toHaveBeenCalledTimes(0);
+    expect(exercicio4.concatString('Ola, ', 'tudo ', 'bem')).toBe('Ola, tudo bem');
+    expect(exercicio4.concatString).toHaveBeenCalled();
+    expect(exercicio4.concatString).toHaveBeenCalledTimes(1);
   })
 })
 
+// Utilizando as mesmas funções do exercício anterior, repita a implementação para a primeira função. Após repetir a implementação, restaure a implementação original e crie os testes necessários para validar. 
 
+it('primeira função', () => {
+  exercicio4.upperCase.mockReset();
+  const mockUpperCase = jest
+    .spyOn(exercicio4, 'upperCase')
+    .mockImplementation((string) => string.toLowerCase());
+
+  expect(mockUpperCase).toHaveBeenCalledTimes(0);
+  expect(mockUpperCase('OLA TudO bEm')).toBe('ola tudo bem');
+  expect(mockUpperCase).toHaveBeenCalled();
+  expect(mockUpperCase).toHaveBeenCalledTimes(1);
+  
+  exercicio4.upperCase.mockRestore();
+
+  expect(exercicio4.upperCase('Ola tudO Bem')).toBe(undefined);
+  expect(exercicio4.upperCase).toHaveBeenCalledTimes(1);
+})
