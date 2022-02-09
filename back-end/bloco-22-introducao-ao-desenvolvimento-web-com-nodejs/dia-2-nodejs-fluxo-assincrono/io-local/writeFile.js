a1a78e668ac5bcbe454d464eba0e02204dc1c78d
alexandrexpj <alexandrexpjr@hotmail.com>
2022-02-09 15:22:31 -0300

    anotacoes aula 22.2

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/io-local/writeFile.js
@@ -0,0 +1,20 @@
+const fs = require('fs').promises;
+
+fs.writeFile('./meu-arquivo.txt', 'Meu textão')
+  .then(() => {
+    console.log('Arquivo escrito com sucesso!');
+  })
+  .catch((err) => {
+    console.error(`Erro ao escrever o arquivo: ${err.message}`);
+  });
+
+async function main() {
+  try {
+    await fs.writeFile('./meu-arquivo.txt', 'Meu textão');
+    console.log('Arquivo escrito com sucesso!');
+  } catch (err) {
+    console.error(`Erro ao escrever o arquivo: ${err.message}`);
+  }
+}
+
+main()
