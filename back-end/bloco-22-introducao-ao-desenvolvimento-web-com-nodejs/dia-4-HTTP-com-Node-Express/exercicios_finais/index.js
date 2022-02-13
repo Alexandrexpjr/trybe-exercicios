a2df7b8586c6ee74a61855cd6caa386aba629a88
alexandrexpj <alexandrexpjr@hotmail.com>
2022-02-13 13:13:30 -0300

    finaliza exercicios

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-HTTP-com-Node-Express/exercicios_finais/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-HTTP-com-Node-Express/exercicios_finais/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const findSimpson = require('./controllers/findSImpson');
+const simpsonsUtils = require('./controllers/fs-utils');
 
 const app = express();
 app.use(express.json());
@@ -28,6 +29,47 @@ app.put('/users/:name/:age', (req, res) => {
   const { name, age } = req.params;
   res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade`})
 });
+
+app.get('/simpsons', async (req, res) => {
+  try {
+    const getSimpsons = await simpsonsUtils.getSimpsons();
+    return res.status(200).json(getSimpsons)
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error'})
+  }
+})
+
+app.get('/simpsons/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const simpson = await findSimpson(id);
+
+    if (!simpson) throw new Error();
+
+    return res.status(200).json(simpson);
+  } catch(err) {
+    return res.status(404).json({ message: 'Not Found'});
+  }
+});
+
+app.post('/simpsons', async (req, res) => {
+  const { id, name } = req.body;
+  const simpsons = await simpsonsUtils.getSimpsons();
+
+  try {
+    const simpson = await findSimpson(id);
+
+    if(simpson) throw new Error();
+
+    const newSimpson = { id, name };
+    console.log([...simpsons, newSimpson])
+    await simpsonsUtils.setSimpsons([...simpsons, newSimpson]);
+    return res.status(204).end();
+  } catch (err) {
+    return res.status(409).json({ message: 'id already exists'})
+  }
+
+})
 
 app.listen(3001, () => console.log('App ouvindo na porta 3001'));
 
