7f4d288dabe490ccfbc6a2423b02487f8c90600a
alexandrexpj <alexandrexpjr@hotmail.com>
2022-02-20 19:36:17 -0300

    finaliza CRUD 23.1

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios_finais/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios_finais/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { errorMid } = require('./middlewares/user');
-const { create } = require('./models/User');
+const { create, listUsers, findUser, change } = require('./models/User');
 
 const app = express();
 
@@ -10,6 +10,34 @@ app.post('/user', errorMid, async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
   const createdUser = await create({firstName, lastName, email, password});
   return res.status(201).json(createdUser);
+});
+
+app.get('/user', async (req, res) => {
+  const users = await listUsers();
+  return res.status(200).json(users);
+});
+
+app.get('/user/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const userFound = await findUser(id);
+    if (userFound.length === 0) return res.status(404).json({ "error": true, "message": "Usuário não encontrado" });
+    return res.status(200).json(userFound);
+  } catch (err) {
+    return res.status(500).json({ "message": "Algo deu errado :(" });
+  }
+})
+
+app.put('/user/:id', errorMid, async(req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, email, password } = req.body;
+  try {
+    const changedUser = await change({ id, firstName, lastName, email, password });
+    if (changedUser.length === 0) return res.status(404).json({ "error": true, "message": "Usuário não encontrado" });
+    return res.status(200).json(changedUser);
+  } catch (err) {
+    return res.status(500).json({ "message": "Algo deu errado :(" });
+  }
 })
 
 
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios_finais/models/User.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios_finais/models/User.js
@@ -18,9 +18,30 @@ async function create({ firstName, lastName, email, password }) {
     lastName,
     email,
   }
+};
+
+async function listUsers() {
+  const query = 'SELECT id, first_name, last_name, email FROM users';
+  const [ getUsers ] = await connection.execute(query);
+  return getUsers.map(formatUser)
+};
+
+async function findUser(id) {
+  const query = 'SELECT id, first_name, last_name, email FROM users WHERE id=?';
+  const [ getUser ] = await connection.execute(query, [parseInt(id)]);
+  return getUser.map(formatUser);
+};
+
+async function change({ id, firstName, lastName, email, password }) {
+  const query = `UPDATE users SET first_name = ?, last_name = ?, email = ?, password = ? WHERE id = ?`;
+  await connection.execute(query, [firstName, lastName, email, password, id]);
+  return await findUser(id)
 }
 
 module.exports={
   formatUser,
-  create
+  create,
+  listUsers,
+  findUser,
+  change,
 }
