7abac0e08ce4474ad34fc84a0b15d723b732e47c
alexandrexpj <alexandrexpjr@hotmail.com>
2022-03-07 15:43:45 -0300

    exercicios fixacao

---
+++ back-end/bloco-24-ORM-e-autenticacao/dia-01-ORM-Interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/models/user.js
@@ -0,0 +1,10 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    // aqui inserimos o datatype da coluna criada
+    phone_num: DataTypes.STRING,
+  });
+
+  return User;
+}
