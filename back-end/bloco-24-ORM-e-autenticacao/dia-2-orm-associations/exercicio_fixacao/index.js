58d5ffdb6aabd50ff9f12cfdf8a993f1af0da251
alexandrexpj <alexandrexpjr@hotmail.com>
2022-03-08 19:01:46 -0300

    exercicios fixacao

---
+++ back-end/bloco-24-ORM-e-autenticacao/dia-2-orm-associations/exercicio_fixacao/index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const { Address, Employee } = require('./models');
+const { Book, User } = require('./models');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/employees', async (_req, res) => {
+  try {
+   const employees = await Employee.findAll({
+     include: { model: Address, as: 'addresses' },
+   });
+
+    return res.status(200).json(employees);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  };
+});
+
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findOne({ where: { id } });
+
+    if (!employee)
+      return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    if (req.query.includeAddresses === 'true') {
+      const addresses = await Address.findAll({ where: { employeeId: id } });
+      return res.status(200).json({ employee, addresses });
+    }
+
+    return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+app.post('/employees', async (req, res) => {
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    const employee = await Employee.create({ firstName, lastName, age });
+
+    await Address.create({ city, street, number, employeeId: employee.id });
+
+    return res.status(201).json({ message: 'Cadastrado com sucesso' });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+// Users e books
+
+app.get('/usersbooks/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({
+      where: { userId: id },
+      include: [{ model: Book, as: 'books', through: { attributes: [] } }],
+    });
+
+    if (!user)
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
